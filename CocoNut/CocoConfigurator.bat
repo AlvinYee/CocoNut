6ee5f90e5ee239ec6f3035356ce993e7b78ff103
Alvin Yee <changchang.ye@hotmail.com>
2018-04-10 14:25:16 +0800

    Update CocoConfigurator.bat

--- CocoNut/CocoConfigurator.bat
+++ CocoNut/CocoConfigurator.bat
@@ -1,7 +1,7 @@
 @echo off
 echo ************************************************************************
-echo [INFO]: CocoNut.Cococonfigurator starts
-echo [version]: 1.1
+echo [INFO]: CocoNut.CocoConfigurator starts
+echo [version]: 1.0
 echo ************************************************************************
 
 set _HDL_RMH_Cfg=..\template\_HDL_RMH_Cfg.h
@@ -14,21 +14,18 @@ set HDL_TMH_Cfg=..\output\HDL_TMH_Cfg.h
 set HCM_LCD_Cfg=..\output\HCM_LCD_Cfg.h
 set I2C2CAN_Par=..\output\I2C2CAN_Par.h
 
-
 if not exist %_HDL_RMH_Cfg% goto ErrorWrongPath
 if not exist %_HDL_TMH_Cfg% goto ErrorWrongPath
 if not exist %_HCM_LCD_Cfg% goto ErrorWrongPath
-if not exist %_I2C2CAN_Par% goto ErrorWrongPath
 
 if not exist %HDL_RMH_Cfg% cd.> %HDL_RMH_Cfg%
 if not exist %HDL_TMH_Cfg% cd.> %HDL_TMH_Cfg%
 if not exist %HCM_LCD_Cfg% cd.> %HCM_LCD_Cfg%
-if not exist %I2C2CAN_Par% cd.> %I2C2CAN_Par%
 
 python -m cogapp -d -o %HDL_RMH_Cfg% %_HDL_RMH_Cfg%
 python -m cogapp -d -o %HDL_TMH_Cfg% %_HDL_TMH_Cfg%
 python -m cogapp -d -o %HCM_LCD_Cfg% %_HCM_LCD_Cfg%
-python -m cogapp -d -o %I2C2CAN_Par% %_I2C2CAN_Par%
+
 goto End
 :ErrorWrongPath
 echo ************************************************************************
@@ -37,6 +34,6 @@ echo ************************************************************************
 
 :End
 echo ************************************************************************
-echo [INFO]: CocoNut.Cococonfigurator ends
+echo [INFO]: CocoNut.CocoConfigurator ends
 echo ************************************************************************
 pause
